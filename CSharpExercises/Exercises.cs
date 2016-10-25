9833fd2c2d19cbb9370a4f7d1e65c32a0247e0ad
Egor Perekupenko <perekupenkoegor@yahoo.com>
2016-10-25 16:27:10 -0700

    cSharpExercises

--- CSharpExercises/Exercises.cs
+++ CSharpExercises/Exercises.cs
@@ -18,12 +18,73 @@ namespace CSharpExercises
         /* Alright - your turn now! */
 
         // 1. Create a method called SayHello that accepts a string representing a name and returns a welcome message (E.g. "Hello John!")
+        public static string SayHello(string John)
+        {
+            return "Hello John!";
+        }
+
         // 2. Create a method called Sum that accepts two integers and returns their sum.
+        public static int Sum(int firstNumber,int secondNumber )
+        {
+            firstNumber = 10;
+            secondNumber = 12;
+            int Sum = firstNumber + secondNumber;
+            return Sum ;
+        }
         // 3. Create a method called Divide that accepts two decimals and returns the result of dividing the two numbers as a decimal.
+        public static decimal Divide(decimal firstNumber, decimal secondNumber)
+        {
+            firstNumber = 10;
+            secondNumber = 5;
+            decimal  Divide = firstNumber / secondNumber;
+            return Divide;
+        }
         // 4. Create a method called CanOpenCheckingAccount that accepts an integer representing a customers age, returning true if the age is greater than or equal to 18, or false if the argument is less than 18.
+        public static bool CanOpenCheckingAccount(int customerAge)
+        {
+            int Age = 16;
+            if (Age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
         // 5. Create a method called GetFirstName that accepts a string representing a full name (e.g. "John Smith"), and returns only the first name.
+        public static string GetFirstName(string fullName) {
+            string  Name = "John Smith";
+            var names = Name.Split(' ');
+            string firstName = names[0];
+            string lastName = names[1];
+            return firstName;
+        }
+
         // 6. Create a method called ReverseStringHard that accepts a string and returns the string in reverse. (No built in functions allowed)
+        public static string ReverseStringHard(string String)
+        {
+            char[] charArray = String.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
         // 7. Create a method called ReverseStringEasy that accepts a string and returns the string in reverse. (Using only built in functions)
+        public static string ReverseStringEasy(string String)
+        {
+            string value = "";
+            string reverse = string.Empty;
+
+            char chSplit = ' ';
+
+            string[] words = value.Split(new char[] { chSplit }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                reverse = word + chSplit + reverse;
+            }
+            return reverse;
+        }
         // 8. Create a method called PrintTimesTable that accepts an integer and returns the times table as a string for that number up to the 10th multiplication.
         /* e.g. 10 should return
          * 10 * 1 = 10
@@ -38,8 +99,31 @@ namespace CSharpExercises
          * 10 * 10 = 10 */
 
         // 9. Create a method called ConvertKelvinToFahrenheit that accepts a double representing a temperature in kelvin and returns a double containing the temperature in Fahrenheit.
+      public  static double ConvertKelvinToFahrenheit(double f )
+        {
+
+            double Fahrenheit = Math.Round((9.0 / 5) * (f - 273.15) + 32, 2);
+            
+            return Fahrenheit;
+        }
+
         // 10. Create a method called GetAverageHard that accepts an array of integers and returns the average value as a double. (No built in functions allowed)
+        public static double GetAverageHard(   int [] myArr)
+        {
+            double[] array1 = { 10, 20, 30, 40, 50, 60, 80 };
+            double average1 = array1.Average();
+            return average1;
+        }
+
         // 11. Create a method called GetAverageEasy that accepts an array of integers and returns the average value as a double. (Using only built in functions)
+
+        // public static double  GetAveregeEasy( int[] myarr)
+        // {
+        //   int sum = Sum(myarr);
+        // double result = (double)sum / myarr.Length;
+        // return result;
+        //}
+
         // 12. Create a method called DrawTriangle that accepts two integers - number and width and returns a string containing a drawn triangle using the number parameter.
         /* e.g. Number: 8, Width: 8 should return
          * 88888888
@@ -54,7 +138,33 @@ namespace CSharpExercises
         // 13. Create a method called GetMilesPerHour that accepts a double representing distance and three integers representing hours, minutes and seconds. The method should return the speed in MPH rounded to the nearest whole number as a string. (e.g. "55MPH")
 
 
+
+
+
+
+
+
+
         // 14. Create a method called IsVowel that accepts a char parameter and returns true if the parameter is a vowel or false if the parameter is a consonant.
+        public static bool IsVowel(char param)
+        {
+            char c = 'a';
+
+           
+           bool isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+
+
+
+            Convert.ToBoolean(ConvertTo);
+            if (isLowercaseVowel = c)
+                return true;
+            else
+                
+            return false ;
+        }
+
+    
         // 15. Create a method called IsConsonant that accepts a char parameter and returns true if the parameter is a consonant or false if the parameter is a vowel.
         // 16. The Collatz conjecture, named after Lothar Collatz of Germany, proposed the following conjecture in 1937. 
         // Beginning with an integer n > 1, repeat the following until n == 1. If n is even, halve it. If n is odd, triple it and add 1. Following these steps, the function will always arrive at the number 1.
